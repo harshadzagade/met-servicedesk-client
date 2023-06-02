670d1588701b7b81e5b094e38bcd9347e53d5b09
Siddharth Bhat <dbgt4531@gmail.com>
2023-06-02 11:42:53 +0530

    searching done

--- src/Components/Staff/SuperAdmin/AllStaffList/AllStaffList.jsx
+++ src/Components/Staff/SuperAdmin/AllStaffList/AllStaffList.jsx
@@ -10,7 +10,9 @@ const AllStaffList = () => {
     const id = localStorage.getItem('id');
     const navigate = useNavigate();
     const [staffList, setStaffList] = useState([]);
-
+    const [allStaffList, setAllStaffList] = useState(staffList);
+    const [searchText, setSearchText] = useState('');
+    const [searchType, setSearchType] = useState('Firstname');
     const [currentPageData, setCurrentPageData] = useState(new Array(0).fill());
     const [numberOfPages, setNumberOfPages] = useState(10);
     const [refresh, setRefresh] = useState(false);
@@ -32,44 +34,95 @@ const AllStaffList = () => {
         setRefresh(false);
     }, [id, refresh]);
 
+    useEffect(() => {
+        let arr = [];
+        switch (searchType) {
+            case 'Firstname':
+                staffList.filter((a) => a.firstname.startsWith(searchText)).map((data) => {
+                    return (
+                        arr.push(data)
+                    );
+                });
+                break;
+
+            case 'Lastname':
+                staffList.filter((a) => a.lastname.startsWith(searchText)).map((data) => {
+                    return (
+                        arr.push(data)
+                    );
+                });
+                break;
+
+            case 'E-Mail':
+                staffList.filter((a) => a.email.startsWith(searchText)).map((data) => {
+                    return (
+                        arr.push(data)
+                    );
+                });
+                break;
+
+            default:
+                break;
+        }
+        if (searchText.length !== 0) {
+            setAllStaffList(arr);
+        } else {
+            setAllStaffList(staffList)
+        }
+    }, [searchText, staffList, searchType]);
+
     return (
         <Fragment>
-            {
-                staffList.length > 0 ?
-                <Fragment>
-                    <DataPerPage numberOfPages={numberOfPages} setNumberOfPages={setNumberOfPages} />
-                    <table className={`${classes.tableParent}`}>
-                        <thead className={`${classes.tableHeader}`}>
-                            <tr className={`${classes.tableRow}`}>
-                                <th className={`${classes.tableHead}`} scope="col">Name</th>
-                                <th className={`${classes.tableHead}`} scope="col">E-Mail</th>
-                                <th className={`${classes.tableHead}`} scope="col">Role</th>
-                                <th className={`${classes.tableHead}`} scope="col">Department</th>
-                            </tr>
-                        </thead>
-                        <tbody className={`${classes.tableBody}`}>
-                            {
-                                currentPageData.length > 0 && currentPageData.map((field) => (
-                                    <tr className={`${classes.tableField} ${classes.tableRow}`} key={field.id}>
-                                        <td className={`${classes.tableData}`} data-label="name" onClick={() => navigate(`/superadmin/${field.id}`)}>{field.firstname + ' ' + field.lastname}</td>
-                                        <td className={`${classes.tableData}`} data-label="email" onClick={() => navigate(`/superadmin/${field.id}`)}>{field.email}</td>
-                                        <td className={`${classes.tableData}`} data-label="role" onClick={() => navigate(`/superadmin/${field.id}`)}>{field.role}</td>
-                                        <td className={`${classes.tableData}`} data-label="department" onClick={() => navigate(`/superadmin/${field.id}`)}>{field.department}</td>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>
 
-                    <SweetPagination
-                        currentPageData={setCurrentPageData}
-                        dataPerPage={numberOfPages}
-                        getData={staffList}
-                        navigation={true}
-                    />
-                </Fragment>
-                :
-                <div className={`${classes.homeNoData}`}>No staff added</div>
+            <DataPerPage numberOfPages={numberOfPages} setNumberOfPages={setNumberOfPages} />
+            <div className='mt-3'>
+                <input type="text" className={`${classes.searchInput}`} onChange={(e) => setSearchText(e.target.value)} />
+                <div className="btn-group mb-1">
+                    <button type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {searchType}
+                    </button>
+                    <div className="dropdown-menu">
+                        <div className="dropdown-item" onClick={() => setSearchType('Firstname')}>Firstname</div>
+                        <div className="dropdown-item" onClick={() => setSearchType('Lastname')}>Lastname</div>
+                        <div className="dropdown-item" onClick={() => setSearchType('E-Mail')}>E-Mail</div>
+                    </div>
+                </div>
+            </div>
+            {
+                allStaffList.length > 0 ?
+                    <Fragment>
+                        <table className={`${classes.tableParent}`}>
+                            <thead className={`${classes.tableHeader}`}>
+                                <tr className={`${classes.tableRow}`}>
+                                    <th className={`${classes.tableHead}`} scope="col">Name</th>
+                                    <th className={`${classes.tableHead}`} scope="col">E-Mail</th>
+                                    <th className={`${classes.tableHead}`} scope="col">Role</th>
+                                    <th className={`${classes.tableHead}`} scope="col">Department</th>
+                                </tr>
+                            </thead>
+                            <tbody className={`${classes.tableBody}`}>
+                                {
+                                    currentPageData.length > 0 && currentPageData.map((field) => (
+                                        <tr className={`${classes.tableField} ${classes.tableRow}`} key={field.id}>
+                                            <td className={`${classes.tableData}`} data-label="name" onClick={() => navigate(`/superadmin/${field.id}`)}>{field.firstname + ' ' + field.lastname}</td>
+                                            <td className={`${classes.tableData}`} data-label="email" onClick={() => navigate(`/superadmin/${field.id}`)}>{field.email}</td>
+                                            <td className={`${classes.tableData}`} data-label="role" onClick={() => navigate(`/superadmin/${field.id}`)}>{field.role}</td>
+                                            <td className={`${classes.tableData}`} data-label="department" onClick={() => navigate(`/superadmin/${field.id}`)}>{field.department}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+
+                        <SweetPagination
+                            currentPageData={setCurrentPageData}
+                            dataPerPage={numberOfPages}
+                            getData={allStaffList}
+                            navigation={true}
+                        />
+                    </Fragment>
+                    :
+                    <div className={`${classes.homeNoData}`}>No staff added</div>
             }
         </Fragment>
     );
