fec5faa3cf3137a34d065c30fea7ee01a75db0c5
Siddharth Bhat <dbgt4531@gmail.com>
2023-06-13 10:21:02 +0530

    complaint filters added

--- src/Components/ComplaintPage/SuperAdminComplaint/AllStaffComplaint/AllStaffComplaint.jsx
+++ src/Components/ComplaintPage/SuperAdminComplaint/AllStaffComplaint/AllStaffComplaint.jsx
@@ -4,21 +4,57 @@ import classes from './AllStaffComplaint.module.css';
 import RequestDetails from '../ComplaintDetails/ComplaintDetails';
 import DataPerPage from '../../../UI/DataPerPage/DataPerPage';
 import Sweetpagination from 'sweetpagination';
+import Swal from 'sweetalert2';
 
 const AllStaffComplaint = () => {
-    const [staffComplaintList, setStaffComplaintList] = useState([]);
+    const [complaintList, setComplaintList] = useState([]);
     const [numberOfPages, setNumberOfPages] = useState(10);
     const [currentPageData, setCurrentPageData] = useState(new Array(0).fill());
     const [searchType, setSearchType] = useState('Subject');
     const [searchText, setSearchText] = useState('');
-    const [allStaffComplaintList, setAllStaffComplaintList] = useState(staffComplaintList);
+    const [allComplaintList, setAllComplaintList] = useState(complaintList);
     const [openDetails, setOpenDetails] = useState(false);
     const [detailsId, setDetailsId] = useState(null);
+    const [isNormalSearch, setIsNormalSearch] = useState(true);
+    const [department, setDepartment] = useState('');
+    const [departments, setDepartments] = useState([]);
+    const [openDepartmentList, setOpenDepartmentList] = useState(false);
+    const [category, setCategory] = useState('');
+    const [categories, setCategories] = useState([]);
+    const [openCategoryList, setOpenCategoryList] = useState(false);
+    const [priority, setPriority] = useState('');
+    const [openPriorityList, setOpenPriorityList] = useState(false);
+    const [status, setStatus] = useState('');
+    const [openStatusList, setOpenStatusList] = useState(false);
+
+    useEffect(() => {
+        const getDepartments = async () => {
+            const departments = await axios.get(`http://localhost:8001/api/complaint/complaintdepartments`);
+            setDepartments(departments.data.departments);
+        };
+        getDepartments();
+    }, [openDepartmentList]);
+
+    useEffect(() => {
+        const getCategories = async () => {
+            const categories = await axios.get(`http://localhost:8001/api/complaint/complaintcategories`);
+            setCategories(categories.data.categories);
+        };
+        getCategories();
+    }, [openCategoryList]);
 
     useEffect(() => {
         const getList = async () => {
-            const list = await axios.get(`http://localhost:8001/api/complaint/allcomplaints`);
-            setStaffComplaintList(list.data.complaints);
+            try {
+                const list = await axios.get(`http://localhost:8001/api/complaint/allcomplaints`);
+                setComplaintList(list.data.complaints);
+            } catch (error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: `${error.response.data.message}`,
+                    text: 'Unable to fetch complaints'
+                });
+            }
         };
         getList();
     }, []);
@@ -33,10 +69,103 @@ const AllStaffComplaint = () => {
     };
 
     useEffect(() => {
+        const getComplaintByDepartment = async () => {
+            try {
+                if ((openDepartmentList && department.length === 0) || (openDepartmentList && department === 'allDepartments')) {
+                    setAllComplaintList(complaintList);
+                } else {
+                    const complaints = await axios.get(`http://localhost:8001/api/complaint/complaintsbydepartment/${department}`);
+                    setAllComplaintList(complaints.data.complaints);
+                }
+            } catch (error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: `${error.response.data.message}`,
+                    text: 'Unable to fetch complaints'
+                });
+            }
+        };
+        if (openDepartmentList) {
+            getComplaintByDepartment();
+        }
+    }, [department, openDepartmentList, complaintList]);
+
+    useEffect(() => {
+        const getComplaintByCategory = async () => {
+            try {
+                if ((openCategoryList && category.length === 0) || (openCategoryList && category === 'allCategories')) {
+                    setAllComplaintList(complaintList);
+                } else {
+                    const complaints = await axios.get(`http://localhost:8001/api/complaint/complaintsbycategory/${category}`);
+                    setAllComplaintList(complaints.data.complaints);
+                }
+            } catch (error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: `${error.response.data.message}`,
+                    text: 'Unable to fetch complaints'
+                });
+            }
+        };
+        if (openCategoryList) {
+            getComplaintByCategory();
+        }
+    }, [category, openCategoryList, complaintList]);
+
+    useEffect(() => {
+        const getComplaintByPriority = async () => {
+            try {
+                if ((openPriorityList && priority.length === 0) || (openPriorityList && priority === 'allPriorities')) {
+                    setAllComplaintList(complaintList);
+                } else {
+                    const complaints = await axios.get(`http://localhost:8001/api/complaint/complaintsbypriority/${priority}`);
+                    setAllComplaintList(complaints.data.complaints);
+                }
+            } catch (error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: `${error.response.data.message}`,
+                    text: 'Unable to fetch complaints'
+                });
+            }
+        };
+        if (openPriorityList) {
+            getComplaintByPriority();
+        }
+    }, [priority, openPriorityList, complaintList]);
+
+    useEffect(() => {
+        const getComplaintByStatus = async () => {
+            try {
+                if ((openStatusList && status.length === 0) || (openStatusList && status === 'allStatus')) {
+                    setAllComplaintList(complaintList);
+                } else {
+                    const complaints = await axios.get(`http://localhost:8001/api/complaint/complaintsbystatus/${status}`);
+                    setAllComplaintList(complaints.data.complaints);
+                }
+            } catch (error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: `${error.response.data.message}`,
+                    text: 'Unable to fetch complaints'
+                });
+            }
+        };
+        if (openStatusList) {
+            getComplaintByStatus();
+        }
+    }, [status, openStatusList, complaintList]);
+
+    useEffect(() => {
         let arr = [];
         switch (searchType) {
             case 'Subject':
-                staffComplaintList.filter((a) => a.subject.startsWith(searchText)).map((data) => {
+                setOpenDepartmentList(false);
+                setOpenCategoryList(false);
+                setOpenPriorityList(false);
+                setOpenStatusList(false);
+                setIsNormalSearch(true);
+                complaintList.filter((a) => a.subject.startsWith(searchText)).map((data) => {
                     return (
                         arr.push(data)
                     );
@@ -44,7 +173,12 @@ const AllStaffComplaint = () => {
                 break;
 
             case 'Name':
-                staffComplaintList.filter((a) => a.name.startsWith(searchText)).map((data) => {
+                setOpenDepartmentList(false);
+                setOpenCategoryList(false);
+                setOpenPriorityList(false);
+                setOpenStatusList(false);
+                setIsNormalSearch(true);
+                complaintList.filter((a) => a.name.startsWith(searchText)).map((data) => {
                     return (
                         arr.push(data)
                     );
@@ -52,52 +186,99 @@ const AllStaffComplaint = () => {
                 break;
 
             case 'Department':
-                staffComplaintList.filter((a) => a.department.startsWith(searchText)).map((data) => {
-                    return (
-                        arr.push(data)
-                    );
-                });
+                setOpenDepartmentList(true);
+                setOpenCategoryList(false);
+                setOpenPriorityList(false);
+                setOpenStatusList(false);
+                setIsNormalSearch(false);
                 break;
 
             case 'Category':
-                staffComplaintList.filter((a) => a.category.startsWith(searchText)).map((data) => {
-                    return (
-                        arr.push(data)
-                    );
-                });
+                setOpenDepartmentList(false);
+                setOpenCategoryList(true);
+                setOpenPriorityList(false);
+                setOpenStatusList(false);
+                setIsNormalSearch(false);
                 break;
 
             case 'Priority':
-                staffComplaintList.filter((a) => a.priority.startsWith(searchText)).map((data) => {
-                    return (
-                        arr.push(data)
-                    );
-                });
+                setOpenDepartmentList(false);
+                setOpenCategoryList(false);
+                setOpenPriorityList(true);
+                setOpenStatusList(false);
+                setIsNormalSearch(false);
                 break;
 
             case 'Status':
-                staffComplaintList.filter((a) => a.status.startsWith(searchText)).map((data) => {
-                    return (
-                        arr.push(data)
-                    );
-                });
+                setOpenDepartmentList(false);
+                setOpenCategoryList(false);
+                setOpenPriorityList(false);
+                setOpenStatusList(true);
+                setIsNormalSearch(false);
                 break;
 
             default:
                 break;
         }
         if (searchText.length !== 0) {
-            setAllStaffComplaintList(arr);
+            setAllComplaintList(arr);
         } else {
-            setAllStaffComplaintList(staffComplaintList)
+            setAllComplaintList(complaintList)
         }
-    }, [searchText, staffComplaintList, searchType]);
+    }, [searchText, complaintList, searchType]);
 
     return (
         <Fragment>
             {openDetails && <RequestDetails onConfirm={handleUpdateCancel} id={detailsId} />}
             <DataPerPage numberOfPages={numberOfPages} setNumberOfPages={setNumberOfPages} />
-            <input type="text" className={`${classes.searchInput}`} placeholder={`Please search ${searchType}`} onChange={(e) => setSearchText(e.target.value)} />
+            {isNormalSearch && <input type="text" className={`${classes.searchInput}`} placeholder={`Please search ${searchType}`} onChange={(e) => setSearchText(e.target.value)} />}
+            {
+                openDepartmentList &&
+                <select value={department} className={`${classes.optionSearchBox}`} name="departments" required onChange={(e) => setDepartment(e.target.value)}>
+                    <option value='' hidden>Select Your Department</option>
+                    <option value={'allDepartments'}>All Departments</option>
+                    {
+                        departments.map((department, key) => (
+                            <option key={key} value={department}>{department}</option>
+                        ))
+                    }
+                </select>
+            }
+            {
+                openCategoryList &&
+                <select value={category} className={`${classes.optionSearchBox}`} name="categories" required onChange={(e) => setCategory(e.target.value)}>
+                    <option value='' hidden>Select Your Category</option>
+                    <option value={'allCategories'}>All Categories</option>
+                    {
+                        categories.map((category, key) => (
+                            <option key={key} value={category}>{category}</option>
+                        ))
+                    }
+                </select>
+            }
+            {
+                openPriorityList &&
+                <select value={priority} className={`${classes.optionSearchBox}`} name="priorities" required onChange={(e) => setPriority(e.target.value)}>
+                    <option value='' hidden>Select Your Priority</option>
+                    <option value='allPriorities'>All Priorities</option>
+                    <option value='high'>High</option>
+                    <option value='moderate'>Moderate</option>
+                    <option value='low'>Low</option>
+                </select>
+            }
+            {
+                openStatusList &&
+                <select value={status} className={`${classes.optionSearchBox}`} name="status" required onChange={(e) => setStatus(e.target.value)}>
+                    <option value='' hidden>Select Your Status</option>
+                    <option value='allStatus'>All Status</option>
+                    <option value='pending'>Pending</option>
+                    <option value='assigned'>Assigned</option>
+                    <option value='attending'>Attending</option>
+                    <option value='forwarded'>Forwarded</option>
+                    <option value='closed'>Closed</option>
+                    <option value='disapproved'>Disapproved</option>
+                </select>
+            }
             <div className="btn-group mb-1">
                 <button type="button" className={`${classes.searchButton} dropdown-toggle`} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     {searchType}
@@ -112,7 +293,7 @@ const AllStaffComplaint = () => {
                 </div>
             </div>
             {
-                allStaffComplaintList.length > 0 ?
+                allComplaintList.length > 0 ?
                     <Fragment>
                         <table className={`${classes.tableParent}`}>
                             <thead className={`${classes.tableHeader}`}>
@@ -144,7 +325,7 @@ const AllStaffComplaint = () => {
                         <Sweetpagination
                             currentPageData={setCurrentPageData}
                             dataPerPage={numberOfPages}
-                            getData={allStaffComplaintList}
+                            getData={allComplaintList}
                             navigation={true}
                         />
 
