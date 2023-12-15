d175d7d01f9a82dd09672e5a1587669ee78b061c
Siddharth Bhat <dbgt4531@gmail.com>
2023-12-15 12:09:59 +0530

    differentiators added

--- src/components/pages/complaint/Complaint.jsx
+++ src/components/pages/complaint/Complaint.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
+import AuthContext from '../../../context/AuthContext/AuthContext';
+import Superadmin from './superadmin/Superadmin';
+import Admin from './admin/Admin';
+import Subadmin from './subadmin/Subadmin';
+import Engineer from './engineer/Engineer';
+import User from './user/User';
 
 const Complaint = () => {
+    const authCtx = useContext(AuthContext);
+    const role = authCtx.employeeInfo ? authCtx.employeeInfo.role : 'checking...';
     return (
-        <div>Complaint</div>
+        <Fragment>
+            { role === 'superadmin' && <Superadmin /> }
+            { role === 'admin' && <Admin /> }
+            { role === 'subadmin' && <Subadmin /> }
+            { role === 'engineer' && <Engineer /> }
+            { role === 'user' && <User /> }
+        </Fragment>
     );
 };
 
--- src/components/pages/report/Report.jsx
+++ src/components/pages/report/Report.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
+import AuthContext from '../../../context/AuthContext/AuthContext';
+import Superadmin from './superadmin/Superadmin';
+import Admin from './admin/Admin';
+import Subadmin from './subadmin/Subadmin';
+import Engineer from './engineer/Engineer';
 
 const Report = () => {
+    const authCtx = useContext(AuthContext);
+    const role = authCtx.employeeInfo ? authCtx.employeeInfo.role : 'checking...';
     return (
-        <div>Report</div>
+        <Fragment>
+            { role === 'superadmin' && <Superadmin /> }
+            { role === 'admin' && <Admin /> }
+            { role === 'subadmin' && <Subadmin /> }
+            { role === 'engineer' && <Engineer /> }
+        </Fragment>
     );
 };
 
