4dbb4ae49f6a148efd17effc1e86bffde1d8da9c
Siddharth Bhat <dbgt4531@gmail.com>
2023-12-18 14:47:33 +0530

    removed policies checks

--- src/components/pages/policies/Policies.jsx
+++ src/components/pages/policies/Policies.jsx
@@ -1,22 +1,8 @@
-import React, { Fragment, useContext } from 'react';
-import AuthContext from '../../../context/AuthContext/AuthContext';
-import Superadmin from './superadmin/Superadmin';
-import Admin from './admin/Admin';
-import Subadmin from './subadmin/Subadmin';
-import Engineer from './engineer/Engineer';
-import User from './user/User';
+import React from 'react';
 
 const Policies = () => {
-    const authCtx = useContext(AuthContext);
-    const role = authCtx.employeeInfo ? authCtx.employeeInfo.role : 'checking...';
     return (
-        <Fragment>
-            { role === 'superadmin' && <Superadmin /> }
-            { role === 'admin' && <Admin /> }
-            { role === 'subadmin' && <Subadmin /> }
-            { role === 'engineer' && <Engineer /> }
-            { role === 'user' && <User /> }
-        </Fragment>
+        <div>Policies</div>
     );
 };
 
--- src/components/pages/policies/subadmin/Subadmin.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Subadmin = () => {
-    return (
-        <div>Subadmin</div>
-    );
-};
-
-export default Subadmin;
